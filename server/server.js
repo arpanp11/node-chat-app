50952dd3cd7f6a844337ac94b2e40d1c233ecc21
Arpan Patel <arpanpatel@Arpans-MacBook-Pro.local>
2018-10-28 14:13:59 -0400

    Add geolocation support via Geolocation API

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-var {generateMessage} = require('./utils/message');
+var { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join('__dirname', '../public');
 const port = process.env.PORT || 3000;
@@ -14,28 +14,26 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
-  
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App.'));
-  
-    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-  
-    socket.on('createMessage', (message) => {
-      console.log('createMessage', message);
-      io.emit('newMessage', generateMessage(message.from, message.text));
-      // socket.broadcast.emit('newMessage', {
-      //   from: message.from,
-      //   text: message.text,
-      //   createdAt: new Date().getTime()
-      // });
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('User was disconnected');
-    });
+  console.log('New user connected');
+
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat App.'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+
   });
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}.`);
+  console.log(`Server is up on port ${port}.`);
 });
 
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require("expect");
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,5 +11,18 @@ describe('generateMessage', () => {
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({ from, text });
 
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Jay';
+        var latitude = 25;
+        var longitude = 50;
+        var url = 'https://www.google.com/maps/?q=25,50';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({ from, url });
     });
 });
